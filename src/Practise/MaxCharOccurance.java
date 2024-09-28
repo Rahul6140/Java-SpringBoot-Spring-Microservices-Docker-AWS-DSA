1de5afc638ac87370c20cc0ab74b670cb62a7f3c
Rahul Yadav <rahuly.yadav0@gmail.com>
2024-09-28 22:48:17 +0530

    1

--- src/Practise/MaxCharOccurance.java
+++ src/Practise/MaxCharOccurance.java
@@ -1,5 +1,6 @@
 package Practise;
 
+import java.net.StandardSocketOptions;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,9 +26,38 @@ public class MaxCharOccurance {
         System.out.println("Character : "+ans);
         System.out.println("Count : "+count);
     }
+
+    private static int getCharCount(String str, char ch){
+        String s= str.toLowerCase().trim();
+        int count = 0;
+        count =(int) s.chars().filter(c->c==ch).count();
+        return count;
+    }
+
+    private static void getCharAndCountForAll(String str) {
+        Map<Character, Integer> mp = new HashMap<>();
+        String s = str.toLowerCase().replaceAll("\\s", "");
+        for(char c : s.toCharArray()){
+            mp.put(c, mp.getOrDefault(c,0)+1);
+
+        }
+        for (Map.Entry<Character, Integer> entry : mp.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
     public static void main(String[] arg){
         String str = "Rahul yadavvvv";
         getCharAndCount(str);
+
+        //Using lambda expression to count charactor
+        System.out.println("Using Lambda expression");
+        char ch = 'h';
+        int count = getCharCount(str, ch);
+        System.out.println("Char "+ch+" count : "+count);
+
+        System.out.println("Printing count of all characters");
+        getCharAndCountForAll(str);
     }
 
 }
--- src/Practise/PyramidPattern1.java
+++ src/Practise/PyramidPattern1.java
@@ -1,5 +1,7 @@
 package Practise;
 
+import java.util.Scanner;
+
 /* Pyramid Pattern 1 code
 
             1
@@ -14,4 +16,26 @@ package Practise;
 
 */
 public class PyramidPattern1 {
+
+    public static void pyramid(int num){
+        for(int i=1; i<=num; i++){
+            //space
+            for(int j=i; j<=num; j++){
+                System.out.print(" ");
+            }
+
+            for(int k=1;k<=i; k++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the height of pyramid : ");
+        int num = sc.nextInt();
+        sc.close();
+        pyramid(num);
+    }
 }
