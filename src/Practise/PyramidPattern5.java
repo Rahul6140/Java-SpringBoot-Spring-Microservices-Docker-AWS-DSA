df96ac932e69e2e2ae4bf27ed895b8d9b171d148
Rahul Yadav <rahuly.yadav0@gmail.com>
2024-09-29 11:29:31 +0530

    pattern

--- src/Practise/PyramidPattern5.java
+++ src/Practise/PyramidPattern5.java
@@ -14,5 +14,31 @@ package Practise;
 
  */
 
+import java.util.Scanner;
+
 public class PyramidPattern5 {
+
+    public static void pyramid(int n){
+        for(int i=n; i>=1; i--){
+            //space
+            for(int j=i; j<=n; j++){
+                System.out.print("  ");
+            }
+            for(int k=1; k<=i; k++){
+                System.out.print(k+" ");
+            }
+            for(int l=i-1;l>=1;l--){
+                System.out.print(l+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Height of Pyramid : ");
+        int n = sc.nextInt();
+        sc.close();
+        pyramid(n);
+    }
 }
--- src/Practise/PyramidPattern6.java
+++ src/Practise/PyramidPattern6.java
@@ -14,5 +14,32 @@ package Practise;
 
  */
 
+import java.util.Scanner;
+
 public class PyramidPattern6 {
+
+    public static void pyramid(int num){
+        for(int i=num; i>=1; i--){
+            //space
+            for(int j=1; j<=i; j++){
+                System.out.print("  ");
+            }
+            for(int k=i; k<=num; k++){
+                System.out.print(k+" ");
+            }
+            for(int l=num-1; l>=i; l--){
+                System.out.print(l+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter height of pyramid : ");
+        int num = sc.nextInt();
+        sc.close();
+        pyramid(num);
+    }
+
 }
